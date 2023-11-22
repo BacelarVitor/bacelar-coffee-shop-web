4ea9e374b86cfcbcfebf642a5aed385f0a1a7298
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-11-21 21:53:46 -0300

    cria hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    .hero {
+        background-color: #394046;
+        background-image: url("img/sascha-eremin-DNQ-M93tHmA-unsplash-3000x1000.jpg");
+        background-position: center;
+        background-size: cover;
+    }
+</style>
